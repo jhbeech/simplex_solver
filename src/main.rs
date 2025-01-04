e8d2fa3e82562a709dcace287592879129b622fd
Joseph Beech <joseph.h.beech@gmail.com>
2025-01-04 12:23:13 +0800

    file that exists

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use simplex_solver::{primal_simplex, read_matrices_from_json};
 
 fn main() {
     let mat =
-        read_matrices_from_json("./benches/data/biggestExample.json").expect("data should be here");
+        read_matrices_from_json("./benches/data/bigExample.json").expect("data should be here");
     let mut basis = primal_simplex(mat.a, mat.b, mat.c, 10000);
     basis.sort();
     assert_eq!(basis, mat.sol, "Test failed",);
